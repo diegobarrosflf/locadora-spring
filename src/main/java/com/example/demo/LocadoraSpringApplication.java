e4088bf761cc32ccd47d9fea4894171a7cba38c0
Diego Barros <diegobarrosflf@gmail.com>
2020-06-10 15:57:15 -0300

    Criando operação de instanciacao

--- src/main/java/com/example/demo/LocadoraSpringApplication.java
+++ src/main/java/com/example/demo/LocadoraSpringApplication.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LocadoraSpringApplication {
+import com.example.demo.domain.Categoria;
+import com.example.demo.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class LocadoraSpringApplication implements CommandLineRunner{
+
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LocadoraSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 =  new Categoria(null,"Ficção");
+		Categoria cat2 =  new Categoria(null,"Ação");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
 	}
 
 }
--- src/main/java/com/example/demo/resource/CategoriaResource.java
+++ src/main/java/com/example/demo/resource/CategoriaResource.java
@@ -14,7 +14,7 @@ import com.example.demo.domain.Categoria;
 import com.example.demo.service.CategoriaService;
 
 @RestController
-@RequestMapping(value = "/categorias")
+@RequestMapping(value = "/categoria")
 public class CategoriaResource {
 	
 	@Autowired
