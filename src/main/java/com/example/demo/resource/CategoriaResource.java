df7a1c41d6fdad054bce181907ff32ed80921dcf
Diego Barros <diegobarrosflf@gmail.com>
2020-06-10 15:09:49 -0300

    Criando repository e service para Categoria

--- src/main/java/com/example/demo/resource/CategoriaResource.java
+++ src/main/java/com/example/demo/resource/CategoriaResource.java
@@ -3,27 +3,27 @@ package com.example.demo.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.service.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
-	@GetMapping
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Terror");
-		Categoria cat2 = new Categoria(2, "Comédia");
-		
-		List<Categoria> categorias = new ArrayList<>();
-		categorias.add(cat1);
-		categorias.add(cat2);
-		
-		return categorias;
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id); 
+		return ResponseEntity.ok().body(obj);
 		
 	}
 
---
+++ src/main/java/com/example/demo/service/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Categoria;
+import com.example.demo.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
