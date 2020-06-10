5b39d99cea4f84259ff03f618ebe363aca34354e
Diego Barros <diegobarrosflf@gmail.com>
2020-06-10 16:38:32 -0300

    Filme associação muitos para muitos

--- src/main/java/com/example/demo/LocadoraSpringApplication.java
+++ src/main/java/com/example/demo/LocadoraSpringApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.domain.Filme;
 import com.example.demo.repository.CategoriaRepository;
+import com.example.demo.repository.FilmeRepository;
 
 @SpringBootApplication
 public class LocadoraSpringApplication implements CommandLineRunner{
@@ -17,6 +19,9 @@ public class LocadoraSpringApplication implements CommandLineRunner{
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private FilmeRepository filmeRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LocadoraSpringApplication.class, args);
 	}
@@ -24,10 +29,22 @@ public class LocadoraSpringApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Categoria cat1 =  new Categoria(null,"Ficção");
-		Categoria cat2 =  new Categoria(null,"Ação");
+		Categoria cat1 =  new Categoria(null,"Ação");
+		Categoria cat2 =  new Categoria(null,"Terror");
+		
+		Filme f1 = new Filme(null, "Matrix", 5.00);
+		Filme f2 = new Filme(null, "O chamado", 6.00);
+		Filme f3 = new Filme(null, "Exterminador do futuro", 4.50);
+		
+		cat1.getFilmes().addAll(Arrays.asList(f1,f2,f3));
+		cat2.getFilmes().addAll(Arrays.asList(f2));
+		
+		f1.getCategorias().addAll(Arrays.asList(cat1));
+		f2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		f3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		filmeRepository.saveAll(Arrays.asList(f1,f2,f3));
 		
 		
 	}
--- src/main/java/com/example/demo/domain/Filme.java
+++ src/main/java/com/example/demo/domain/Filme.java
@@ -8,28 +8,36 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
+public class Filme implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 
-	@ManyToMany(mappedBy = "categorias")
-	private List<Filme> filmes = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name = "FILME_CATEGORIA", 
+		joinColumns = @JoinColumn(name = "filme_id"),
+		inverseJoinColumns = @JoinColumn(name = "categoria_id")
+			)
+	private List<Categoria> categorias = new ArrayList<>();
 
-	public Categoria() {
+	public Filme() {
 
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Filme(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -48,12 +56,20 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
-	public List<Filme> getFilmes() {
-		return filmes;
+	public Double getPreco() {
+		return preco;
 	}
 
-	public void setFilmes(List<Filme> filmes) {
-		this.filmes = filmes;
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
@@ -61,7 +77,6 @@ public class Categoria implements Serializable {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		return result;
 	}
 
@@ -73,16 +88,11 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Filme other = (Filme) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
-			return false;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
 			return false;
 		return true;
 	}
