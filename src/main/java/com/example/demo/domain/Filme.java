2b7cccf39d13880b75b1fb97d892ef66ae789eac
Diego Barros <diegobarrosflf@gmail.com>
2020-06-10 18:01:30 -0300

    Ajustes no endpoit /categorias/{id}

--- src/main/java/com/example/demo/domain/Filme.java
+++ src/main/java/com/example/demo/domain/Filme.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Filme implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -22,6 +24,7 @@ public class Filme implements Serializable {
 	private String nome;
 	private Double preco;
 
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "FILME_CATEGORIA", 
 		joinColumns = @JoinColumn(name = "filme_id"),
--- src/main/java/com/example/demo/service/CategoriaService.java
+++ src/main/java/com/example/demo/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Categoria;
 import com.example.demo.repository.CategoriaRepository;
+import com.example.demo.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo:" + Categoria.class.getName()));
 	}
 
 }
